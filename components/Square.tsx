73b199e0783d1891c087f39e02594216c2f875c1
Bryan <cortinabryan@gmail.com>
2021-11-10 13:09:27 -0800

    styled and added tictactoe logic

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,5 +1,23 @@
-function Square() {
-  return <button>This is a square</button>;
+type Player = "X" | "O" | "BOTH" | null;
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button className="square" onClick={onClick} disabled={Boolean(winner)} />
+    );
+  }
+  return (
+    <button className={`square square_${value.toLocaleLowerCase()}`} disabled>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
